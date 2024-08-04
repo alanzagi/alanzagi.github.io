3c96fd5f82a7b7a0ef856205ecbbb338c9914956
Alan <alanzagi2007@gmail.com>
2024-08-05 00:35:40 +0700

    menambahkan sweetalert

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 // Fungsi untuk mengambil apapun data yang diinputkan di Pesan Whatsapp
 document.getElementById("message-submit-button").addEventListener("click", function () {
   const inputText = document.getElementById("message-input").value.trim(); // Menghapus spasi tambahan
+
+  // Memeriksa apakah input kosong
+  if (inputText === "") {
+    Swal.fire({
+      icon: "error",
+      title: "Gagal mengirim",
+      text: "Harap isi semua bidang sebelum mengirim pesan",
+      confirmButtonColor: "#f44336", // Ganti dengan warna yang diinginkan
+      confirmButtonText: "OK",
+    });
+    return; // Menghentikan eksekusi jika input kosong
+  }
+
   const whatsappUrl = `https://wa.me/6281521550913?text=${encodeURIComponent(inputText)}`;
   window.open(whatsappUrl, "_blank");
 });
@@ -10,8 +23,20 @@ document.getElementById("message-submit-button2").addEventListener("click", func
   const nameInput = document.getElementById("name-input").value.trim();
   const messageInput = document.getElementById("message-input2").value.trim();
 
+  // Memeriksa apakah input kosong
+  if (nameInput === "" || messageInput === "") {
+    Swal.fire({
+      icon: "error",
+      title: "Gagal mengirim",
+      text: "Harap isi semua bidang sebelum mengirim pesan",
+      confirmButtonColor: "#f44336", // Ganti dengan warna yang diinginkan
+      confirmButtonText: "OK",
+    });
+    return; // Menghentikan eksekusi jika input kosong
+  }
+
   // Membuat URL WhatsApp
-  const whatsappUrl = `https://wa.me/6281521550913?text=${encodeURIComponent("Halo, Nama saya " + nameInput + " dan tujuan saya adalah " + messageInput)}`;
+  const whatsappUrl = `https://wa.me/6281521550913?text=${encodeURIComponent(nameInput + ": " + messageInput)}`;
 
   // Membuka URL di tab baru
   window.open(whatsappUrl, "_blank");
